31b302f45565fa2cf07b3321af9658b57fcbb73a
ahmedbaklouti <45592600+ahmedbaklouti@users.noreply.github.com>
2023-01-18 22:58:12 +0100

    test

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const routes = require('./hello');
+
+app.use('/', routes);
+
+const PORT = process.env.PORT || 5050;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
