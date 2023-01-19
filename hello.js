6e8cae9aae51caa212cb95278b2c7b6572e9e3a2
ahmedbaklouti <45592600+ahmedbaklouti@users.noreply.github.com>
2023-01-19 21:46:55 +0100

    test

--- hello.js
+++ hello.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello World!", test: "ok" });
+  res.status(200).json({ message: "Hello World!", test: "ok ok" });
 });
 
 module.exports = router;
