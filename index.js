16e1116795594cb674b3fa0a6cdd9684e1de20f3
ahmedbaklouti <45592600+ahmedbaklouti@users.noreply.github.com>
2023-01-24 22:52:23 +0100

    test front

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+const path = require('path');
 const express = require("express");
 const app = express();
 const routes = require('./hello');
 
-app.use('/', routes);
+app.use('/api', routes);
+
+app.use(express.static(path.join(__dirname, 'frontend/build')));
+
+app.get('*', (req, res) =>
+  res.sendFile(
+    path.resolve(__dirname, 'frontend', 'build', 'index.html')
+  )
+);
 
 const PORT = process.env.PORT || 5050;
 
